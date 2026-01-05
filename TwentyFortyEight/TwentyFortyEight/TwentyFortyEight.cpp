9baae791794ed410f4ada8ca4895d091301dd039
Razeihk <rasec.tracip@gmail.com>
2026-01-05 18:55:00 +0100

    Check empty slots

--- TwentyFortyEight/TwentyFortyEight/TwentyFortyEight.cpp
+++ TwentyFortyEight/TwentyFortyEight/TwentyFortyEight.cpp
@@ -1,44 +1,113 @@
-// TwentyFortyEight.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <ctime>
+#include <random>
 
 #include "TwentyFortyEight.h"
 
 using namespace std;
 
-namespace twentyFortyEightConst
-{
-	constexpr int c_numberOfRows = 4;
-	constexpr int c_numberOfColumns = 4;
-}
-
 
 int main()
 {
-	twentyFortyEight();
+	InitialiseGrid();
 }
 
-void twentyFortyEight()
+void InitialiseGrid()
 {
-	int grid[twentyFortyEightConst::c_numberOfRows][twentyFortyEightConst::c_numberOfColumns];
+	RandomSeed = (int)time(0);	// (int) asks to cast the variable as an int before returning it
+								// This initialises the randomSeed with a different number each time the app starts
 
-	for (size_t i = 0; i < twentyFortyEightConst::c_numberOfColumns; i++)
+	for (int i = 0; i < TFEConst::c_NumberOfRows; i++)
 	{
-		for (size_t u = 0; u < twentyFortyEightConst::c_numberOfRows; u++)
+		for (int u = 0; u < TFEConst::c_NumberOfColumns; u++)
 		{
-			grid[i][u] = 0;
+			Grid[i][u] = 0;
+		}
+	}
+	
+	for (int i = 0; i < 20; i++)
+	{
+		GenerateNewNumbers();
+	}
 
-			if (u < twentyFortyEightConst::c_numberOfRows - 1)
+	DisplayGrid();
+}
+
+void DisplayGrid()
+{
+	for (int i = 0; i < TFEConst::c_NumberOfRows; i++)
+	{
+		for (int u = 0; u < TFEConst::c_NumberOfColumns; u++)
+		{
+
+			if (u < TFEConst::c_NumberOfColumns - 1)
 			{
-				cout << grid[i][u] << " | ";
+				cout << Grid[i][u] << " | ";
 			}
 			else
 			{
-				cout << grid[i][u] << endl;
+				cout << Grid[i][u] << endl;
+			}
+		}
+	}
+}
+
+void GenerateNewNumbers()
+{
+	UpdateEmptySlots();
+
+	if (EmptySlots.empty()) // Check if there's a slot available
+	{
+		cout << "You lost you loser" << endl; // Call losing function instead
+		return;
+	}
+
+	int SelectedSlot = RandomInt(0, (int)EmptySlots.size() - 1);
+	
+	int tempRow = EmptySlots[SelectedSlot].first;
+	int tempColumn = EmptySlots[SelectedSlot].second;
+
+	cout << tempRow << " and " << tempColumn << endl;
+
+	Grid[tempRow][tempColumn] = 2;
+
+	RandomSeed++;
+}
+
+void MoveNumbers()
+{
+	// g peur
+}
+
+int RandomInt(int Min, int Max)
+{
+	// Complicated function from Zazar, no idea how it works (he doesn't know either)
+	static random_device RandomDevice;
+	static mt19937 Eng(RandomDevice());
+	uniform_int_distribution<int> Distribution(Min, Max);
+
+
+	return Distribution(Eng);
+}
+
+void UpdateEmptySlots()
+{
+	EmptySlots.clear(); // Reset the vector of empty slots
+
+	for (int i = 0; i < TFEConst::c_NumberOfRows; i++)
+	{
+		for (int u = 0; u < TFEConst::c_NumberOfColumns; u++)
+		{
+			if (Grid[i][u] == 0)
+			{
+				EmptySlots.push_back(pair<int, int>(i, u));
 			}
 		}
 	}
 
-	//grid[rand()][rand()] = 2;
+	// Display available slots
+	/*for (size_t i = 0; i < (int)EmptySlots.size(); i++)
+	{
+		cout << EmptySlots[i].first << "," << EmptySlots[i].second << endl;
+	}*/
 }
--- TwentyFortyEight/TwentyFortyEight/TwentyFortyEight.h
+++ TwentyFortyEight/TwentyFortyEight/TwentyFortyEight.h
@@ -1,4 +1,24 @@
 #pragma once
+#include <vector>
 
-void twentyFortyEight();
-void twentyFortyEightDisplay();
+namespace TFEConst
+{
+	constexpr int c_NumberOfColumns = 4;
+	constexpr int c_NumberOfRows = 4;
+}
+
+
+//FUNCTIONS
+
+void InitialiseGrid();
+void DisplayGrid();
+void GenerateNewNumbers();
+void UpdateEmptySlots();
+int RandomInt(int Min, int Max);
+
+
+// VARIABLES
+
+int Grid[TFEConst::c_NumberOfColumns][TFEConst::c_NumberOfRows];
+int RandomSeed = 0;
+std::vector<std::pair<int, int>> EmptySlots; // std::pair contains 2 objects tbd (first and second)
